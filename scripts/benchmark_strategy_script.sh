293285317b9f9d5e83a275846994f7d0e82e78d9
Joel Bengs <joel.bengs@ericsson.com>
2024-06-10 12:52:03 +0000

    plots produced for powerpoint

--- scripts/benchmark_strategy_script.sh
+++ scripts/benchmark_strategy_script.sh
@@ -11,11 +11,11 @@ export OMP_NUM_THREADS=$((nb_cpu_threads / nproc_per_node))
 # Note: Time to execute was the same if this was =1 or =32
 
 backbones=(
-#any:all:all:all
-any:all_but_neck:all:all
+any:all:all:all
+#any:all_but_neck:all:all
 )
 # the protection of MBConvs is hardcoded into the ops.py definitions
-# WAAAAARNING THE FLAGS ARE SET TO TRUE as they should be in this case
+# WAAAAARNING THE FLAGS ARE SET TO False
 
 prompts=(
 #box
@@ -30,48 +30,48 @@ xl0_quant
 xl1_quant
 )
 
-#echo "--------- Benchmarking script on COCO, only Full MBCONV protected ---------}"
-#
-#for backbone_item in "${backbones[@]}"
-#do
-#    for prompt in "${prompts[@]}"
-#    do
-#        for model in "${models[@]}"
-#        do
-#            echo " "
-#            # Run the evaluation command for the current model - with --quantize and configurations
-#            torchrun --nproc_per_node=2 \
-#            eval_sam_model_joel.py \
-#            --dataset coco \
-#            --image_root coco/val2017 \
-#            --dataset_calibration sa-1b \
-#            --image_root_calibration sa-1b \
-#            --annotation_json_file coco/annotations/lvis_v1_val.json \
-#            --annotation_json_file coco/annotations/instances_val2017.json \
-#            --source_json_file coco/source_json_file/coco_vitdet.json \
-#            --limit_iterations 4500 \
-#            --model $model \
-#            --prompt_type $prompt \
-#            --backbone_version $backbone_item \
-#            --quantize_W \
-#            --quantize_A \
-#            --script_name benchmark
-#            # --limit_iterations 10 \
-#            # --export_dataframe \
-#            # --print_progress \
-#            # --plot_distributions \
-#            # --quantize_method_W $qmw \
-#            # --quantize_N \ # NOT NORMALLY USED
-#            # --quantize_A \
-#            # --observer_method_W $obsmethod \
-#            # --observer_method_A $obsmethod \
-#            # --print_torchinfo \
-#            echo "$prompt COCO: $model, $backbone_item" §
-#        done
-#    done
-#done
+echo "--------- Benchmarking script on COCO, INT8 everything ---------}"
 
-echo "--------- Benchmarking script on LVIS, both box and box_from_detector. ---------}"
+for backbone_item in "${backbones[@]}"
+do
+    for prompt in "${prompts[@]}"
+    do
+        for model in "${models[@]}"
+        do
+            echo " "
+            # Run the evaluation command for the current model - with --quantize and configurations
+            torchrun --nproc_per_node=2 \
+            eval_sam_model_joel.py \
+            --dataset coco \
+            --image_root coco/val2017 \
+            --dataset_calibration sa-1b \
+            --image_root_calibration sa-1b \
+            --annotation_json_file coco/annotations/lvis_v1_val.json \
+            --annotation_json_file coco/annotations/instances_val2017.json \
+            --source_json_file coco/source_json_file/coco_vitdet.json \
+            --limit_iterations 4500 \
+            --model $model \
+            --prompt_type $prompt \
+            --backbone_version $backbone_item \
+            --quantize_W \
+            --quantize_A \
+            --script_name benchmark
+            # --limit_iterations 10 \
+            # --export_dataframe \
+            # --print_progress \
+            # --plot_distributions \
+            # --quantize_method_W $qmw \
+            # --quantize_N \ # NOT NORMALLY USED
+            # --quantize_A \
+            # --observer_method_W $obsmethod \
+            # --observer_method_A $obsmethod \
+            # --print_torchinfo \
+            echo "$prompt COCO: $model, $backbone_item" §
+        done
+    done
+done
+
+echo "--------- Benchmarking script on LVIS, INT8 everything ---------}"
 
 for backbone_item in "${backbones[@]}"
 do
